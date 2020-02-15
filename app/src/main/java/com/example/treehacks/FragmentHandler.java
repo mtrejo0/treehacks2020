c6bb520a1c2b2d66ed8c8618703fe249af09839a
Moises Trejo <moisest@mit.edu>
2020-02-14 20:38:26 -0800

    add libary

---
+++ app/src/main/java/com/example/treehacks/FragmentHandler.java
@@ -0,0 +1,45 @@
+package com.example.treehacks;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class FragmentHandler extends AppCompatActivity {
+    FragmentManager fragmentManager;
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        fragmentManager = getSupportFragmentManager();
+
+        // define manager to decide which fragment to display
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = new HomeFragment();
+                switch (item.getItemId()) {
+                    case R.id.home_fragment:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.chat_fragment:
+                        fragment = new ChatFragment();
+                        break;
+                    default:
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+        bottomNavigationView.setSelectedItemId(R.id.home_fragment);
+    }
+}
